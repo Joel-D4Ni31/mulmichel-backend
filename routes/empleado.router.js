cee88b860cc27872b695994d07f4d9aea3b930e8
Joel-D4Ni31 <018201540d@uandina.edu.pe>
2022-11-21 17:50:50 -0500

    commit04-tara03-u3

--- routes/empleado.router.js
+++ routes/empleado.router.js
@@ -1,52 +1,74 @@
 const express = require ('express');
 const router = express.Router();
+
 const EmpleadosService = require('../services/empleados.service')
 const service = new EmpleadosService();
 
-router.get('/', (req, res) => {
+
+const  validatorHandler =  require('../middlewares/validator.handler');
+const {createEmpleadoSchema,updatempleadoSchema,getEmpleadoSchema} = require('../schemas/empleados.schema');
+
+
+router.get('/', async (req, res) => {
   const empleados = service.find();
     res.status(200).json(empleados);
   });
+router.get('/:id',
+           validatorHandler(getEmpleadoSchema,'params'),
+  async (req,res, next)=>{
+try{
+const { id }= req.params;
+const empleado = await service.findOne(id);
+res.status(200).json(empleado);
+}catch(error){
+next(error);
+}
+});
 
 
-  router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const empleado = service.findOne(id);
-    if (empleado === undefined){
-      res.status(404).json({
-        message: 'empleado not found',
-        id
-      });
-    }
-    res.status(200).json(empleado);
-  });
-
-  router.post('/',(req, res) => {
+router.post('/',
+  validatorHandler(createEmpleadoSchema,'body'),
+  async (req,res)=>{
     const body = req.body;
-    const nuevoEmpleado = service.create(body);
-    res.status(201).json( {
+    const nuevoEmpleado = await service.create(body);
+    res.status(201).json({
       message: 'creado',
       nuevoEmpleado
     });
+  })
+
+router.patch('/:id',
+  validatorHandler(getEmpleadoSchema,'params'),
+  validatorHandler(updatempleadoSchema,'body'),
+  async (req,res, next)=>{
+    try{
+      const {id} = req.params;
+      const body = req.body;
+      const empleado = await service.update(id, body);
+      res.status(200).json({
+        message: 'actualizado',
+        empleado
+      });
+    }catch(error){
+      next(error);
+    }
   });
 
-  router.patch('/:id',(req, res) => {
-   const body = req.body;
-   const { id } = req.params;
-   const empleado = service.update(id, body);
-    res.status(200).json( {
+
+router.delete('/:id',
+validatorHandler(getEmpleadoSchema,'params'),
+async (req,res, next)=>{
+  try{
+    const {id} = req.params;
+    const empleado = await service.update(id);
+    res.status(200).json({
       message: 'actualizado',
       empleado
     });
-  });
+  }catch(error){
+    next(error);
+  }
+});
 
-  router.delete('/:id',(req, res) => {
-    const { id } = req.params;
-    const rta= service.delete(id);
-     res.status(200).json( {
-       message: 'eliminado',
-       rta
-     });
-   });
+module.exports= router;
 
-  module.exports= router;
